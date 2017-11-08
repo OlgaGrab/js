527f6c380362f7e2823823aa7169d186fff1295f
Olga <olga_grabchak@i.ua>
2017-11-08 22:29:58 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,28 +1,26 @@
-document.write('<br>'); 
-document.write(userName);
-
-const a = Number(prompt('Input A'));
-const b = Number(prompt('Input B'));
-const c = Number(prompt('Input C'));
+const a = Number(prompt(`Input A`));
+const b = Number(prompt(`Input B`));
+const c = Number(prompt(`Input C`));
  
-
 function solveQuadr(a, b, c) {
-    if (a == 0) {
-        return 'bx+c=0'; 
-    }
-    else if (b == 0) { 
-        return 'ax+c=0'; }
-    else if (c == 0) {
-        return ''
-    }
-    
+     let x, x1, x2;
+    if (d > 0) {
+        return 'x1 = (-b+sqrd)/(2*a)',
+        'x2 = (-b-sqrd)/(2*a)'; }
+        else if (d = 0) {
+        return 'x = -b/(2*a)';
+        }
+        else if (d < 0) {
+        return 'null';
+        }
+        const d = disc(a, b, c);
+        const sqrd = Math.sqrt(d);
+       
+
+function disc(a, b, c) {
+return d = b*b-(4*a*c);
 }
-function disc(D) {
-if (D > 0) {
-    return 'x1,2 =(-b ± √D)/2a';
-    } else if (D == 0) {
-        return 'x = -b/2a';
-    } else if (D < 0) {
-        return 'x1,2 =(-b±i√-D)/2a'
-    }
-}
+
+
+const result = solveQuadr(a, b, c);
+document.write(result);
