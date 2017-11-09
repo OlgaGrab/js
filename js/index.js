fc875b6f62b9a5ca6d8ce0d9362017e415f65a58
Olga <olga_grabchak@i.ua>
2017-11-09 12:24:16 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -3,24 +3,22 @@ const b = Number(prompt(`Input B`));
 const c = Number(prompt(`Input C`));
  
 function solveQuadr(a, b, c) {
-     let x, x1, x2;
+    let x, x1, x2;
+    const d = disc(a, b, c);
     if (d > 0) {
-        return 'x1 = (-b+sqrd)/(2*a)',
-        'x2 = (-b-sqrd)/(2*a)'; }
-        else if (d = 0) {
-        return 'x = -b/(2*a)';
+        const sqrd = Math.sqrt(d);
+        return 'x1 = ' + (-b+sqrd)/(2*a) + ', x2 = ' + (-b-sqrd)/(2*a); }
+        else if (d === 0) {
+        return 'x = ' + -b/(2*a);
         }
         else if (d < 0) {
         return 'null';
         }
-        const d = disc(a, b, c);
-        const sqrd = Math.sqrt(d);
-       
-
-function disc(a, b, c) {
-return d = b*b-(4*a*c);
 }
-
+       
+    function disc(a, b, c) {
+    return b*b-4*a*c;
+}
 
 const result = solveQuadr(a, b, c);
 document.write(result);
